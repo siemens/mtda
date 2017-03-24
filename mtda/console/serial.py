51e378b0999d2bf126542357e1936b09f64fce3d
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2017-03-24 18:57:10 +0100

    Basic console support
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

---
+++ mtda/console/serial.py
@@ -0,0 +1,47 @@
+# System imports
+import abc
+import serial
+
+# Local imports
+from mtda.console.interface import ConsoleInterface
+
+class SerialConsole(ConsoleInterface):
+
+    def __init__(self):
+        self.ser  = None
+        self.port = "/dev/ttyUSB0"
+        self.rate = 115200
+
+    def configure(self, conf):
+        """ Configure this console from the provided configuration"""
+        if 'port' in conf:
+            self.port = conf['port']
+        if 'rate' in conf:
+            self.rate = int(conf['rate'], 10)
+        self.ser = serial.Serial()
+        self.ser.port = self.port
+        self.ser.baudrate = self.rate
+
+    def probe(self):
+        if self.ser is not None:
+            return self.ser.open()
+        else:
+            return False
+
+    def pending(self):
+        """ Return number of pending bytes to read"""
+        if self.ser is not None:
+            return self.ser.inWaiting()
+        else:
+            return 0
+
+    def read(self, n=1):
+        """ Read bytes from the console"""
+        if self.ser is not None:
+            return self.ser.read(n)
+        else:
+            return None
+
+def instantiate():
+    return SerialConsole()
+
