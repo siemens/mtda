5ca7cf7e1b837c178293a839ad4049842d1e7865
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2017-04-01 15:11:44 +0200

    Add "console head" command to return the 1st line from the RX buffer
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

--- mtda/console/logger.py
+++ mtda/console/logger.py
@@ -14,12 +14,22 @@ class ConsoleLogger:
         self.rx_thread = None
         self.rx_queue = bytearray()
         self.rx_buffer = deque(maxlen=1000)
+        self.rx_lock = threading.Lock()
 
     def start(self):
         self.rx_alive = True
         self.rx_thread = threading.Thread(target=self.reader, name='console_rx')
         self.rx_thread.daemon = True
         self.rx_thread.start()
+
+    def head(self):
+        self.rx_lock.acquire()
+        if len(self.rx_buffer) > 0:
+            line = self.rx_buffer.popleft()
+        else:
+            line = None
+        self.rx_lock.release()
+        return line
 
     def process_rx(self, data):
         # Add received data to the RX queue
@@ -28,11 +38,17 @@ class ConsoleLogger:
         while True:
             off = self.rx_queue.find(b'\r', 0)
             if off >= 0:
-                # Add this line to the circular buffer
+                # Extract line from the RX queue
                 line = self.rx_queue[0:off]
-                self.rx_buffer.append(line)
+
+                # Add this line to the circular buffer
+                self.rx_lock.acquire()
+                self.rx_buffer.append(line.decode("utf-8"))
+                self.rx_lock.release()
+
                 # Remove consumed bytes from the queue
                 self.rx_queue = self.rx_queue[off+1:]
+
                 # Write this line to stdout
                 sys.stdout.buffer.write(line)
                 sys.stdout.buffer.flush()
