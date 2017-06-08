58615a138f150ad41fdb179d4aa8d73972b5c246
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2017-06-08 18:50:37 +0200

    Print a status of the controllable USB ports when starting the console
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

--- mtda/usb/switch.py
+++ mtda/usb/switch.py
@@ -3,8 +3,9 @@ import abc
 class UsbSwitch(object):
     __metaclass__ = abc.ABCMeta
 
-    POWERED_OFF = 0
-    POWERED_ON  = 1
+    POWERED_OFF    = 0
+    POWERED_ON     = 1
+    POWERED_UNSURE = 2
 
     @abc.abstractmethod
     def configure(self, conf):
@@ -29,7 +30,7 @@ class UsbSwitch(object):
     @abc.abstractmethod
     def status(self):
         """ Determine the current power state of the USB port"""
-        return self.POWERED_OFF
+        return self.POWERED_UNSURE
 
     @abc.abstractmethod
     def toggle(self):
