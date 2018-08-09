44691380d71a8ee0c76035ec642d3f4442f4a6f1
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2018-08-09 22:16:31 +0200

    mtda/usb/rpi_gpio: add the "enable" configuration
    Some USB switches need the trigger GPIO to be high to enable
    the USB port they control, some others require it to be low.
    The "enable" configuration parameter makes this configurable
    
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

--- mtda/usb/rpi_gpio.py
+++ mtda/usb/rpi_gpio.py
@@ -8,14 +8,23 @@ from mtda.usb.switch import UsbSwitch
 class RPiGpioUsbSwitch(UsbSwitch):
 
     def __init__(self):
-        self.dev = None
-        self.pin = 0
+        self.dev     = None
+        self.pin     = 0
+        self.enable  = GPIO.HIGH
+        self.disable = GPIO.LOW
         GPIO.setwarnings(False)
 
     def configure(self, conf):
         """ Configure this USB switch from the provided configuration"""
         if 'pin' in conf:
             self.pin = int(conf['pin'], 10)
+        if 'enable' in conf:
+            if conf['enable'] == 'high':
+                self.enable  = GPIO.HIGH
+                self.disable = GPIO.LOW
+            elif conf['enable'] == 'low':
+                self.enable  = GPIO.LOW
+                self.disable = GPIO.HIGH
         if self.pin > 0:
             GPIO.setmode(GPIO.BCM)
             GPIO.setup(self.pin, GPIO.OUT)
@@ -26,20 +35,20 @@ class RPiGpioUsbSwitch(UsbSwitch):
 
     def on(self):
         """ Power on the target USB port"""
-        GPIO.output(self.pin, GPIO.LOW)
+        GPIO.output(self.pin, self.enable)
         return self.status() == self.POWERED_ON
 
     def off(self):
         """ Power off the target USB port"""
-        GPIO.output(self.pin, GPIO.HIGH)
+        GPIO.output(self.pin, self.disable)
         return self.status() == self.POWERED_OFF
 
     def status(self):
         """ Determine the current power state of the USB port"""
-        if GPIO.input(self.pin) == 1:
-            return self.POWERED_OFF
-        else:
+        if GPIO.input(self.pin) == self.enable:
             return self.POWERED_ON
+        else:
+            return self.POWERED_OFF
 
     def toggle(self):
         s = self.status()
