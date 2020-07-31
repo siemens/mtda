2d7d259c54f1368d8889188a32a8c59acc31351d
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2020-07-31 09:53:16 +0200

    keyboard: add a qemu controller variant
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

---
+++ mtda/keyboard/qemu.py
@@ -0,0 +1,70 @@
+# System imports
+import abc
+import os
+import sys
+import time
+
+# Local imports
+from mtda.keyboard.controller import KeyboardController
+
+class QemuController(KeyboardController):
+
+    def __init__(self, mtda):
+        self.mtda = mtda
+        self.qemu = mtda.power_controller
+
+    def configure(self, conf):
+        self.mtda.debug(3, "keyboard.qemu.configure()")
+
+    def probe(self):
+        self.mtda.debug(3, "keyboard.qemu.probe()")
+
+        result = True
+
+        self.mtda.debug(3, "keyboard.qemu.probe(): %s" % str(result))
+        return result
+
+    def write_report(self, report):
+        self.mtda.debug(3, "keyboard.qemu.write_report()")
+
+        return self.fd.write(report.encode())
+ 
+    def press(self, key, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.press()")
+
+        result = True
+        while repeat > 0:
+            repeat = repeat - 1
+            self.qemu.cmd("sendkey %s" % key)
+            time.sleep(0.1)
+        return result
+
+    def enter(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.enter()")
+        return self.press("ret", repeat)
+
+    def esc(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.esc()")
+        return self.press("esc", repeat)
+
+    def down(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.down()")
+        return self.press("down", repeat)
+
+    def left(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.left()")
+        return self.press("left", repeat)
+
+    def right(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.right()")
+        return self.press("right", repeat)
+
+    def up(self, repeat=1):
+        self.mtda.debug(3, "keyboard.qemu.up()")
+        return self.press("up", repeat)
+
+    def write(self, str):
+        self.mtda.debug(3, "keyboard.qemu.write()")
+
+def instantiate(mtda):
+   return QemuController(mtda)
