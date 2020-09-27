f36bda9dfd7b80a8b12102779dcbd6b643fb0266
Cedric Hombourger <chombourger@gmail.com>
2020-09-27 05:11:10 -0400

    mtda: add __version__ to support docs
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -6,14 +6,12 @@
 
 # -- Path setup --------------------------------------------------------------
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+# Add the project directory to the path, in order to include it:
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import mtda  # noqa (disables pycodestyle check for this line)
 
 # -- Project information -----------------------------------------------------
 
