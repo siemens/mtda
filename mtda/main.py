ed6d51a76d69c551684dbe84f600f02a9b4fabb5
Vijai Kumar K <Vijaikumar_Kanagarajan@mentor.com>
2020-10-08 15:27:52 +0530

    mtda-cli: Add option to print the MTDA version
    Right now there is no user friendly way to find out the MTDA
    version. Add -v option to print the MTDA version.
    
    Also, add provision to print the host and remote mtda version
    part of target info(mtda-cli -r).
    
    Bump the version to 0.5.1. When quering older versions of mtda
    server, which doesnot yet have this feature implemented, the
    version will be printed as <=0.5.
    
    Signed-off-by: Vijai Kumar K <Vijaikumar_Kanagarajan@mentor.com>

--- mtda/main.py
+++ mtda/main.py
@@ -17,6 +17,7 @@ from mtda.console.logger import ConsoleLogger
 from mtda.console.remote_output import RemoteConsoleOutput
 import mtda.keyboard.controller
 import mtda.power.controller
+from mtda import __version__
 
 _NOPRINT_TRANS_TABLE = {
     i: '.' for i in range(0, sys.maxunicode + 1) if not chr(i).isprintable()
@@ -60,6 +61,7 @@ class MentorTestDeviceAgent:
         self._lock_owner = None
         self._lock_expiry = None
         self._lock_timeout = 5  # Lock timeout (in minutes)
+        self.version = __version__
 
         # Config file in $HOME/.mtda/config
         home = os.getenv('HOME', '')
@@ -69,6 +71,9 @@ class MentorTestDeviceAgent:
         # Config file in /etc/mtda/config
         if os.path.exists('/etc'):
             self.config_files.append(os.path.join('/etc', 'mtda', 'config'))
+
+    def agent_version(self):
+        return self.version
 
     def command(self, args, session=None):
         self.mtda.debug(3, "main.command()")
