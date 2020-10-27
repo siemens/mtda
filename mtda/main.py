siemens/mtda

commit 29633f79fa0f9411b2352891ef17ee6e1ea722df

Added support for changing the prefix key configuration

mtda/main.py
@@ -24,6 +24,7 @@ _NOPRINT_TRANS_TABLE = {
     i: '.' for i in range(0, sys.maxunicode + 1) if not chr(i).isprintable()
 }
 
+DEFAULT_PREFIX_KEY = 'ctrl-a'
 
 def _make_printable(s):
     return s.translate(_NOPRINT_TRANS_TABLE)
@@ -56,6 +57,7 @@ class MentorTestDeviceAgent:
         self.usb_switches = []
         self.ctrlport = 5556
         self.conport = 5557
+        self.prefix_key = None
         self.is_remote = False
         self.is_server = False
         self.remote = None
@@ -86,6 +88,26 @@ class MentorTestDeviceAgent:
 
         self.mtda.debug(3, "main.command(): %s" % str(result))
         return result
+
+    def console_prefix_key(self):
+        self.mtda.debug(3, "main.console_prefix_key()")
+        return self.prefix_key
+
+    def _prefix_key_code(self, prefix_key):
+        prefix_key = prefix_key.lower()
+        key_dict = {'ctrl-a' : '\x01' , 'ctrl-b' : '\x02' , 'ctrl-c' : '\x03' , 'ctrl-d' : '\x04',
+                    'ctrl-e' : '\x05' , 'ctrl-f' : '\x06' , 'ctrl-g' : '\x07' , 'ctrl-h' : '\x08',
+                    'ctrl-i' : '\x09' , 'ctrl-j' : '\x0A' , 'ctrl-k' : '\x0B' , 'ctrl-l' : '\x0C',
+                    'ctrl-n' : '\x0E' , 'ctrl-o' : '\x0F' , 'ctrl-p' : '\x10' , 'ctrl-q' : '\x11',
+                    'ctrl-r' : '\x12' , 'ctrl-s' : '\x13' , 'ctrl-t' : '\x14' , 'ctrl-u' : '\x15',
+                    'ctrl-v' : '\x16' , 'ctrl-w' : '\x17' , 'ctrl-x' : '\x18' , 'ctrl-y' : '\x19',
+                    'ctrl-z' : '\x1A' }
+
+        if prefix_key in key_dict:
+            key_ascii = key_dict[prefix_key]
+            return key_ascii
+        else:
+            raise ValueError("the prefix key specified %s is not supported" %(prefix_key))
 
     def console_getkey(self):
         self.mtda.debug(3, "main.console_getkey()")
@@ -868,6 +890,8 @@ class MentorTestDeviceAgent:
             self.load_environment(parser)
         if parser.has_section('remote'):
             self.load_remote_config(parser)
+        if parser.has_section('ui'):
+            self.load_ui_config(parser)
         if self.is_remote is False:
             if parser.has_section('power'):
                 self.load_power_config(parser)
@@ -995,6 +1019,10 @@ class MentorTestDeviceAgent:
         else:
             self.remote = None
         self.is_remote = self.remote is not None
+
+    def load_ui_config(self, parser):
+        self.mtda.debug(3, "main.load_ui_config()")
+        self.prefix_key = self._prefix_key_code(parser.get('ui', 'prefix', fallback=DEFAULT_PREFIX_KEY))
 
     def load_usb_config(self, parser):
         self.mtda.debug(3, "main.load_usb_config()")

mtda-cli
@@ -108,10 +108,13 @@ class Application:
 
         client.console_init()
 
+        # Get the value of prefix key
+        prefix_key = client.console_prefix_key()
+
         # Input loop
         while self.exiting is False:
             c = client.console_getkey()
-            if c == '\x01':
+            if c == prefix_key:
                 c = client.console_getkey()
                 self.console_menukey(c)
             else:

mtda.ini
@@ -24,6 +24,17 @@ console = 5557
 host    = localhost
 
 # ---------------------------------------------------------------------------
+# UI settings
+# ---------------------------------------------------------------------------
+# Set "prefix" to the key which should be followed by command key for
+# controling from interactive console.Default value used for prefix is
+# ctrl-a Note:Supported are values between ctrl-a to ctrl-z with
+# exception of ctrl-m
+# ---------------------------------------------------------------------------
+[ui]
+# prefix = ctrl-a
+
+# ---------------------------------------------------------------------------
 # Console settings
 # ---------------------------------------------------------------------------
 # Set "variant" to specify how to access the console interface of the device.
@@ -77,7 +88,7 @@ variant=aviosys_8800
 # Set "variant" to specify which power control device to use. Use one of:
 #    - qemu
 #    - samsung
-#    - usbf 
+#    - usbf
 # ---------------------------------------------------------------------------
 # Note: this section is ignored when connecting to a remote agent
 # ---------------------------------------------------------------------------

mtda/client.py
@@ -37,6 +37,9 @@ class Client:
     def agent_version(self):
         return self._impl.agent_version()
 
+    def console_prefix_key(self):
+        return self._impl.console_prefix_key()
+
     def command(self, args):
         return self._impl.command(args, self._session)
 

commit 5854a3adb18fc8552cd578ce4a63c1c8f0d47f21

main: fix pycodestyle warnings introduced with 29633f7
Fixes: 29633f7
Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

mtda/main.py
@@ -26,6 +26,7 @@ _NOPRINT_TRANS_TABLE = {
 
 DEFAULT_PREFIX_KEY = 'ctrl-a'
 
+
 def _make_printable(s):
     return s.translate(_NOPRINT_TRANS_TABLE)
 
@@ -95,19 +96,22 @@ class MentorTestDeviceAgent:
 
     def _prefix_key_code(self, prefix_key):
         prefix_key = prefix_key.lower()
-        key_dict = {'ctrl-a' : '\x01' , 'ctrl-b' : '\x02' , 'ctrl-c' : '\x03' , 'ctrl-d' : '\x04',
-                    'ctrl-e' : '\x05' , 'ctrl-f' : '\x06' , 'ctrl-g' : '\x07' , 'ctrl-h' : '\x08',
-                    'ctrl-i' : '\x09' , 'ctrl-j' : '\x0A' , 'ctrl-k' : '\x0B' , 'ctrl-l' : '\x0C',
-                    'ctrl-n' : '\x0E' , 'ctrl-o' : '\x0F' , 'ctrl-p' : '\x10' , 'ctrl-q' : '\x11',
-                    'ctrl-r' : '\x12' , 'ctrl-s' : '\x13' , 'ctrl-t' : '\x14' , 'ctrl-u' : '\x15',
-                    'ctrl-v' : '\x16' , 'ctrl-w' : '\x17' , 'ctrl-x' : '\x18' , 'ctrl-y' : '\x19',
-                    'ctrl-z' : '\x1A' }
+        key_dict = {'ctrl-a': '\x01', 'ctrl-b': '\x02', 'ctrl-c': '\x03',
+                    'ctrl-d': '\x04', 'ctrl-e': '\x05', 'ctrl-f': '\x06',
+                    'ctrl-g': '\x07', 'ctrl-h': '\x08', 'ctrl-i': '\x09',
+                    'ctrl-j': '\x0A', 'ctrl-k': '\x0B', 'ctrl-l': '\x0C',
+                    'ctrl-n': '\x0E', 'ctrl-o': '\x0F', 'ctrl-p': '\x10',
+                    'ctrl-q': '\x11', 'ctrl-r': '\x12', 'ctrl-s': '\x13',
+                    'ctrl-t': '\x14', 'ctrl-u': '\x15', 'ctrl-v': '\x16',
+                    'ctrl-w': '\x17', 'ctrl-x': '\x18', 'ctrl-y': '\x19',
+                    'ctrl-z': '\x1A'}
 
         if prefix_key in key_dict:
             key_ascii = key_dict[prefix_key]
             return key_ascii
         else:
-            raise ValueError("the prefix key specified %s is not supported" %(prefix_key))
+            raise ValueError("the prefix key specified '{0}' is not "
+                             "supported".format(prefix_key))
 
     def console_getkey(self):
         self.mtda.debug(3, "main.console_getkey()")
@@ -1022,7 +1026,8 @@ class MentorTestDeviceAgent:
 
     def load_ui_config(self, parser):
         self.mtda.debug(3, "main.load_ui_config()")
-        self.prefix_key = self._prefix_key_code(parser.get('ui', 'prefix', fallback=DEFAULT_PREFIX_KEY))
+        self.prefix_key = self._prefix_key_code(parser.get(
+            'ui', 'prefix', fallback=DEFAULT_PREFIX_KEY))
 
     def load_usb_config(self, parser):
         self.mtda.debug(3, "main.load_usb_config()")