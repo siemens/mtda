646c4b31ecba5e5a984c7dcaea5827052bebaf41
Cedric Hombourger <chombourger@gmail.com>
2020-11-01 05:49:59 -0500

    console/remote_output: kill pep8 warnings
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- mtda/console/remote_output.py
+++ mtda/console/remote_output.py
@@ -7,6 +7,7 @@ from mtda.console.output import ConsoleOutput
 import sys
 import zmq
 
+
 class RemoteConsoleOutput(ConsoleOutput):
 
     def __init__(self, host, port):
@@ -17,10 +18,9 @@ class RemoteConsoleOutput(ConsoleOutput):
     def reader(self):
         context = zmq.Context()
         socket = context.socket(zmq.SUB)
-        socket.connect ("tcp://%s:%s" % (self.host, self.port))
+        socket.connect("tcp://%s:%s" % (self.host, self.port))
         socket.setsockopt(zmq.SUBSCRIBE, b'')
         while True:
             data = socket.recv()
             sys.stdout.buffer.write(data)
             sys.stdout.flush()
-
