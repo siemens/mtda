5a561667a5d5a88bed1e0140d18a340ad5b8ab67
Cedric Hombourger <Cedric_Hombourger@mentor.com>
2020-11-08 15:22:01 -0500

    assistant: initial version of the HomeKit driver
    Signed-off-by: Cedric Hombourger <Cedric_Hombourger@mentor.com>

---
+++ mtda/assistant/homekit.py
@@ -0,0 +1,82 @@
+# ---------------------------------------------------------------------------
+# HomeKit support for MTDA
+# ---------------------------------------------------------------------------
+#
+# This software is a part of MTDA.
+# Copyright (c) Mentor, a Siemens business, 2017-2020
+#
+# ---------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT
+# ---------------------------------------------------------------------------
+
+
+# System imports
+import abc
+from pyhap.accessory import Accessory
+from pyhap.accessory_driver import AccessoryDriver
+import pyhap.const as Category
+
+# Local imports
+from mtda.assistant.assistant import Assistant
+
+
+class PowerSwitch(Accessory):
+    category = Category.CATEGORY_OUTLET
+
+    def __init__(self, mtda, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.mtda = mtda
+
+        serv_switch = self.add_preload_service('Outlet')
+        self.relay_on = serv_switch.configure_char(
+            'On', setter_callback=self.set_relay)
+
+        self.relay_in_use = serv_switch.configure_char(
+            'OutletInUse', setter_callback=self.get_relay_in_use)
+
+    def get_relay(self):
+        return "1" if self.mtda.target_status() == "ON" else 0
+
+    def set_relay(self, state):
+        self.mtda.debug(3, "mtda.assistant.homekit.set_relay()")
+
+        result = self.get_relay()
+        if result != state:
+            if state == 1:
+                self.mtda.target_on('homekit')
+            else:
+                self.mtda.target_off('homekit')
+            result = self.get_relay()
+
+        self.mtda.debug(3, "mtda.assistant.homekit.set_relay(): %s" % str(result))
+        return result
+
+    def get_relay_in_use(self, state):
+        return True
+
+
+class HomeKitAssistant(Assistant):
+
+    def __init__(self, mtda):
+        self.mtda = mtda
+        self.name = "MTDA"
+        self.port = 51826
+
+    def configure(self, conf):
+        if 'name' in conf:
+            self.name = conf['name']
+        if 'port' in conf:
+            self.port = int(conf['port'], 10)
+
+    def probe(self):
+        return True
+
+    def start(self):
+        drv = AccessoryDriver(port=self.port)
+        drv.add_accessory(accessory=PowerSwitch(self.mtda, drv, self.name))
+        drv.start_service()
+
+
+def instantiate(mtda):
+    return HomeKitAssistant(mtda)
