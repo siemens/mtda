b246be4e8d11fbd7826c559b9df82314fb35d6a7
Cedric Hombourger <chombourger@gmail.com>
2020-11-22 05:32:26 -0500

    scripts: upload amd64 packages
    Signed-off-by: Cedric Hombourger <chombourger@gmail.com>

--- scripts/upload-debs.sh
+++ scripts/upload-debs.sh
@@ -29,20 +29,24 @@ MTDA_VERSION=$(python3 -c 'import mtda; print(mtda.__version__)')
 # Abort if a command fails
 set -e
 
-# Replace MTDA variables in config file
-config_json=$(mktemp)
-sed -e "s,@MTDA_DATE@,${MTDA_DATE},g" \
-    -e "s,@MTDA_VERSION@,${MTDA_VERSION},g" \
-    configs/bintray.json >${config_json}
-
-# Copy Debian packages from the Isar build
-mkdir -p deploy-armhf
-find build-armhf/build/tmp/deploy/isar-apt/apt -name *.deb -exec cp {} deploy-armhf/ \;
-
-# Upload them to bintray
-dpl --provider=bintray            \
-    --file="${config_json}"       \
-    --skip_cleanup                \
-    --user="chombourger"          \
-    --key="${BINTRAYAPIKEY}"      \
-    --passphrase="${BINTRAYPASS}"
+for arch in amd64 armhf
+do
+    # Replace MTDA variables in config file
+    config_json=$(mktemp)
+    sed -e "s,@MTDA_DATE@,${MTDA_DATE},g" \
+        -e "s,@MTDA_VERSION@,${MTDA_VERSION},g" \
+        -e "s,@ARCH@,${arch},g" \
+        configs/bintray.json >${config_json}
+    
+    # Copy Debian packages from the Isar build
+    mkdir -p deploy-${arch}
+    find build-${arch}/build/tmp/deploy/isar-apt/apt -name *.deb -exec cp {} deploy-${arch}/ \;
+    
+    # Upload them to bintray
+    dpl --provider=bintray            \
+        --file="${config_json}"       \
+        --skip_cleanup                \
+        --user="chombourger"          \
+        --key="${BINTRAYAPIKEY}"      \
+        --passphrase="${BINTRAYPASS}"
+done
