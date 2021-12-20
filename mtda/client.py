siemens/mtda

commit 1e0f13ba7e28a220404cb50caecb68c6431078b6

mtda/main: Stop self._session_timer and exiting
Stop self._session_timer thread and exit while
pressing ctrl+c

Signed-off-by: bhargavdas <bhargav_das@mentor.com>

mtda/main.py
@@ -1335,6 +1335,12 @@ class MultiTenantDeviceAccess:
         self._session_timer = mtda.utils.RepeatTimer(60, self._session_check)
         self._session_timer.start()
 
+        # Stop the timer thread on ctrl+C
+        def signal_handler(signum, frame):
+            print("Exiting, Thank you for using MTDA ...")
+            self._session_timer.cancel()
+
+        signal.signal(signal.SIGINT, signal_handler)
         return True
 
     def _session_check(self, session=None):

commit 5255a912d3badf9e810f844afcf551c91ddee3f7

main: provide stop() to shutdown the agent
The SIGINT signal handler would not shutdown the agent cleanly and
would not terminate the process when not detached (-n option). Add
stop() to the agent and client API as well as to the console loggers.

Fixes: 1e0f13b
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

mtda-cli
@@ -96,8 +96,13 @@ class Application:
                 server='{}.local.'.format(name))
 
         zc.register_service(info)
-        s.run()
-        zc.unregister_service(info)
+        try:
+            s.run()
+        except KeyboardInterrupt:
+            s.stop()
+            self.agent.stop()
+        finally:
+            zc.unregister_service(info)
 
         return True
 
@@ -768,6 +773,7 @@ class Application:
             if status is False:
                 print('Failed to start the MTDA server!', file=sys.stderr)
                 return False
+            return True
         else:
             # Start our agent
             self.agent = Client(self.remote)

mtda/client.py
@@ -272,6 +272,12 @@ class Client:
     def start(self):
         return self._agent.start()
 
+    def stop(self):
+        if self._agent.remote is not None:
+            self._impl.close()
+        else:
+            self._agent.stop()
+
     def remote(self):
         return self._agent.remote
 

mtda/console/logger.py
@@ -48,6 +48,12 @@ class ConsoleLogger:
         self.rx_thread.daemon = True
         self.rx_thread.start()
 
+    def stop(self):
+        self.pause()
+        with self.rx_lock:
+            self.rx_alive = False
+            self.rx_cond.notify()
+
     def _clear(self):
         self.rx_buffer.clear()
         self.rx_queue = bytearray()

mtda/main.py
@@ -16,7 +16,6 @@ import gevent
 import importlib
 import os
 import queue
-import signal
 import sys
 import threading
 import time
@@ -263,9 +262,12 @@ class MultiTenantDeviceAccess:
             # Stop previous remote console
             if self.console_output is not None:
                 self.console_output.stop()
-            # Create and start our remote console
-            self.console_output = RemoteConsole(host, self.conport, screen)
-            self.console_output.start()
+            if host is not None:
+                # Create and start our remote console
+                self.console_output = RemoteConsole(host, self.conport, screen)
+                self.console_output.start()
+            else:
+                self.console_output = None
 
         self.mtda.debug(3, "main.console_remote(): %s" % str(result))
         return result
@@ -395,10 +397,14 @@ class MultiTenantDeviceAccess:
             # Stop previous remote console
             if self.monitor_output is not None:
                 self.monitor_output.stop()
-            # Create and start our remote console in paused (buffering) state
-            self.monitor_output = RemoteMonitor(host, self.conport, screen)
-            self.monitor_output.pause()
-            self.monitor_output.start()
+            if host is not None:
+                # Create and start our remote console in paused
+                # (i.e. buffering) state
+                self.monitor_output = RemoteMonitor(host, self.conport, screen)
+                self.monitor_output.pause()
+                self.monitor_output.start()
+            else:
+                self.monitor_output = None
 
         self.mtda.debug(3, "main.monitor_remote(): %s" % str(result))
         return result
@@ -1336,13 +1342,37 @@ class MultiTenantDeviceAccess:
         self._session_timer = mtda.utils.RepeatTimer(60, self._session_check)
         self._session_timer.start()
 
-        # Stop the timer thread on ctrl+C
-        def signal_handler(signum, frame):
-            self.mtda.debug(2, "process interrupted, shutting down...")
-            self._session_timer.cancel()
-
-        signal.signal(signal.SIGINT, signal_handler)
         return True
+
+    def stop(self):
+        self.mtda.debug(3, "main.stop()")
+
+        if self.is_remote is True:
+            return True
+
+        # stop sesssion timer
+        self._session_timer.cancel()
+
+        # stop video
+        if self.video is not None:
+            self.video.stop()
+
+        # stop assistant
+        if self.assistant is not None:
+            self.power_monitors.remove(self.assistant)
+            self.assistant.stop()
+
+        # stop monitor console
+        if self.monitor is not None:
+            self.monitor_logger.stop()
+
+        # stop main console
+        if self.console is not None:
+            self.console_logger.stop()
+
+        if self.socket is not None:
+            self.socket.close()
+            self.socket = None
 
     def _session_check(self, session=None):
         self.mtda.debug(3, "main._session_check(%s)" % str(session))