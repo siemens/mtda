0b6e1fe2fd16a5f598cd5e28fc039de3c03d91a1
Cedric Hombourger <cedric.hombourger@siemens.com>
2022-01-06 11:21:35 +0100

    main: make sure the agent is created with a default name
    Starting the agent without a [main] section in the configuration
    would cause an exception as the name attribute was only set from
    load_main_config()
    
    Fixes: f75ca67
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/main.py
+++ mtda/main.py
@@ -63,6 +63,7 @@ class MultiTenantDeviceAccess:
         self.keyboard = None
         self.video = None
         self.mtda = self
+        self.name = socket.gethostname()
         self.assistant = None
         self.power_controller = None
         self.power_on_script = None
@@ -1015,7 +1016,6 @@ class MultiTenantDeviceAccess:
         self.mtda.debug(3, "main.load_main_config()")
 
         # Name of this agent
-        self.name = socket.gethostname()
         self.name = parser.get('main', 'name', fallback=self.name)
 
         self.mtda.debug_level = int(
