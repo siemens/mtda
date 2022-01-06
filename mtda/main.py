siemens/mtda

commit f75ca67aabaa09d61d1f86c103d4d79be254ae70

main: get network name from the configuration
The name to be broadcasted to the network via Zeroconf may now
be set in the [main] section of the configuration (it will
default to the name of the host we are running on).

Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

mtda/main.py
@@ -16,6 +16,7 @@ import gevent
 import importlib
 import os
 import queue
+import socket
 import sys
 import threading
 import time
@@ -1011,6 +1012,10 @@ class MultiTenantDeviceAccess:
 
     def load_main_config(self, parser):
         self.mtda.debug(3, "main.load_main_config()")
+
+        # Name of this agent
+        self.name = socket.gethostname()
+        self.name = parser.get('main', 'name', fallback=self.name)
 
         self.mtda.debug_level = int(
             parser.get('main', 'debug', fallback=self.mtda.debug_level))

mtda-cli
@@ -86,7 +86,7 @@ class Application:
             'description': 'Multi-Tenant Device Access'
         }
         deviceType = CONSTS.MDNS.TYPE
-        name = socket.gethostname()
+        name = self.agent.name
         info = zeroconf.ServiceInfo(
                 type_=deviceType,
                 name='{}.{}'.format(name, deviceType),

mtda.ini
@@ -13,6 +13,11 @@
 # General settings
 # ---------------------------------------------------------------------------
 [main]
+
+# Name of our agent on the network (defaults to hostname)
+# name = hello-mtda
+
+# Debug level, use 0 for no debug prints, up to 3 for more
 debug = 0
 
 # change to 'yes' for 'storage mount' to use fuse (experimental)

commit 4d9abbc8cb15b043c169be6819f383570ce4d456

main: make sure the agent is created with a default name
Starting the agent without a [main] section in the configuration
would cause an exception as the name attribute was only set from
load_main_config()

Fixes: f75ca67
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

mtda/main.py
@@ -65,6 +65,7 @@ class MultiTenantDeviceAccess:
         self.keyboard = None
         self.video = None
         self.mtda = self
+        self.name = socket.gethostname()
         self.assistant = None
         self.power_controller = None
         self.power_on_script = None
@@ -1014,7 +1015,6 @@ class MultiTenantDeviceAccess:
         self.mtda.debug(3, "main.load_main_config()")
 
         # Name of this agent
-        self.name = socket.gethostname()
         self.name = parser.get('main', 'name', fallback=self.name)
 
         self.mtda.debug_level = int(