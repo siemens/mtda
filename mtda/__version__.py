ada1be68542cd4f2971b7cacc845b944516d2f90
Cedric Hombourger <cedric.hombourger@siemens.com>
2022-01-20 07:06:32 +0100

    prepare 0.15
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/__version__.py
+++ mtda/__version__.py
@@ -3,12 +3,12 @@
 # ---------------------------------------------------------------------------
 #
 # This software is a part of MTDA.
-# Copyright (C) 2021 Siemens Digital Industries Software
+# Copyright (C) 2022 Siemens Digital Industries Software
 #
 # ---------------------------------------------------------------------------
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
 __license__ = 'MIT'
-__copyright__ = 'Copyright (C) 2021 Siemens Digital Industries Software'
-__version__ = '0.14'
+__copyright__ = 'Copyright (C) 2022 Siemens Digital Industries Software'
+__version__ = '0.15'
