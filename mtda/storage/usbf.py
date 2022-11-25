siemens/mtda

commit b92d55b7b2eb516633a6a3906440af8fd8416116

storage/usbf: Remove and re-install storage when switching between host and target
This allows to replace the backing file while it is in host mode and
informs the USB gadget about changes to it. Also, the target will not be
able to access it anymore while it is in host mode.

Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

mtda/storage/usbf.py
@@ -94,6 +94,8 @@ class UsbFunctionController(Image):
         self.mtda.debug(3, "storage.usbf.to_host()")
         self.lock.acquire()
 
+        Composite.remove()
+
         self.mode = self.SD_ON_HOST
         result = True
 
@@ -112,6 +114,13 @@ class UsbFunctionController(Image):
 
         if result is True:
             self.mode = self.SD_ON_TARGET
+
+        if self.file is not None:
+            if os.path.exists(self.file) is True:
+                result = Composite.install()
+            else:
+                self.mtda.debug(1, "storage.usbf.to_target(): "
+                                   "{} not found!".format(self.file))
 
         self.lock.release()
         self.mtda.debug(3, "storage.usbf.to_target(): {}".format(result))

commit 4a56400400e94894ac8dc58430a13cadf066f700

storage/usbf: keep composite functions installed
The following use-case was broken with b92d55b:

  storage_to_target()
  target_on()
  ...
  target_off()
  storage_to_host()
  target_on()

This sequence would uninstall all of the USB functions we provide
including the serial function that we use on systems without a
serial port (console.variant=usbf).

Reverts: b92d55b
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

mtda/storage/usbf.py
@@ -95,8 +95,6 @@ class UsbFunctionController(Image):
         self.mtda.debug(3, "storage.usbf.to_host()")
         self.lock.acquire()
 
-        Composite.remove()
-
         self.mode = CONSTS.STORAGE.ON_HOST
         result = True
 
@@ -115,13 +113,6 @@ class UsbFunctionController(Image):
 
         if result is True:
             self.mode = CONSTS.STORAGE.ON_TARGET
-
-        if self.file is not None:
-            if os.path.exists(self.file) is True:
-                result = Composite.install()
-            else:
-                self.mtda.debug(1, "storage.usbf.to_target(): "
-                                   "{} not found!".format(self.file))
 
         self.lock.release()
         self.mtda.debug(3, "storage.usbf.to_target(): {}".format(result))