258748abfffb802db18e33e2717e56c88778b5d1
Cedric Hombourger <cedric.hombourger@siemens.com>
2023-01-03 18:49:26 +0100

    Copyright update for 2023
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/__version__.py
+++ mtda/__version__.py
@@ -3,12 +3,12 @@
 # ---------------------------------------------------------------------------
 #
 # This software is a part of MTDA.
-# Copyright (C) 2022 Siemens Digital Industries Software
+# Copyright (C) 2023 Siemens Digital Industries Software
 #
 # ---------------------------------------------------------------------------
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
 __license__ = 'MIT'
-__copyright__ = 'Copyright (C) 2022 Siemens Digital Industries Software'
+__copyright__ = 'Copyright (C) 2023 Siemens Digital Industries Software'
 __version__ = '0.17'
