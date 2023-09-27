44b71523cd1ffc5a69b227e1cc3af2b4c8384bb1
Felix Moessbauer <felix.moessbauer@siemens.com>
2023-09-27 16:53:43 +0800

    deps: bump pyzmq max version to 25
    pyzmq 25 is the version shipped in debian bookworm and we did not see
    any issues with that version so far. By that, bump the version to also
    support a pip install on debian bookworm.
    
    Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ and testers to remotely access and control hardware devices.
         "pyserial>=2.6",
         "python-daemon>=2.0",
         "pyusb>=1.0",
-        "pyzmq>=15.0,<23.0.0",
+        "pyzmq>=15.0,<25.0.0",
         "psutil",
         "requests",
         "urwid",
