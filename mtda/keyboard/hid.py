b75b4e5015f3568f1f4920c1d8d2a1768e7bc930
Cedric Hombourger <cedric.hombourger@siemens.com>
2024-01-05 11:47:46 +0100

    chore(keyboard): handle special keys in main
    The hid keyboard driver understands <esc>, <left>, <right>, <down>,
    <up> and function keys as special keys. Move this handling in main
    for other drivers (such as qemu) to also handle these special keys
    as users of keyboard.write() may reasonably expect.
    
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/keyboard/hid.py
+++ mtda/keyboard/hid.py
@@ -240,29 +240,6 @@ class HidKeyboardController(KeyboardController):
             '{': 0x2f, '}': 0x30, '|': 0x31, ':': 0x33, '"': 0x34, '~': 0x35,
             '<': 0x36, '>': 0x37, '?': 0x38
         }
-        special_keys = {
-            '<down>': 0x51,
-            '<enter>': 0x28,
-            '<esc>': 0x29,
-            '<f1>': 0x3a,
-            '<f2>': 0x3b,
-            '<f3>': 0x3c,
-            '<f4>': 0x3d,
-            '<f5>': 0x3e,
-            '<f6>': 0x3f,
-            '<f7>': 0x40,
-            '<f8>': 0x41,
-            '<f9>': 0x42,
-            '<f10>': 0x43,
-            '<f11>': 0x44,
-            '<f12>': 0x45,
-            '<left>': 0x50,
-            '<right>': 0x4f,
-            '<up>': 0x52
-        }
-
-        if what in special_keys:
-            return self.press(special_keys[what])
 
         for k in what:
             if k in lower_keys:
