364a782ab920f53cf4f698a3b27e31c72c20415b
Cedric Hombourger <cedric.hombourger@siemens.com>
2025-01-10 11:28:52 +0100

    refactor(client): use dynamic wrappers for remote calls
    Implement __getattr__ in the client module to check if the remote
    MultiTenantDeviceAccess has the requested attribute and if it is
    a callable: a wrapper is then created to pass a session to the
    remote instance. Static wrappers are removed from the client
    module.
    
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/client.py
+++ mtda/client.py
@@ -62,29 +62,20 @@ class Client:
         else:
             self._session = session
 
-    def agent_version(self):
-        return self._impl.agent_version()
+    def __getattr__(self, name):
+        if self._impl is None:
+            return None
 
-    def config_set_power_timeout(self, timeout):
-        return self._impl.config_set_power_timeout(timeout, self._session)
-
-    def config_set_session_timeout(self, timeout):
-        return self._impl.config_set_session_timeout(timeout, self._session)
+        attr = getattr(self._impl, name)
+        if self._session and callable(attr):
+            def wrapper(*args, **kwargs):
+                kwargs['session'] = self._session
+                return attr(*args, **kwargs)
+            return wrapper
+        return attr
 
     def console_prefix_key(self):
         return self._agent.console_prefix_key()
-
-    def command(self, args):
-        return self._impl.command(args, self._session)
-
-    def console_clear(self):
-        return self._impl.console_clear(self._session)
-
-    def console_dump(self):
-        return self._impl.console_dump(self._session)
-
-    def console_flush(self):
-        return self._impl.console_flush(self._session)
 
     def console_getkey(self):
         return self._agent.console_getkey()
@@ -92,77 +83,20 @@ class Client:
     def console_init(self):
         return self._agent.console_init()
 
-    def console_head(self):
-        return self._impl.console_head(self._session)
-
-    def console_lines(self):
-        return self._impl.console_lines(self._session)
-
-    def console_locked(self):
-        return self._impl.console_locked(self._session)
-
-    def console_print(self, data):
-        return self._impl.console_print(data, self._session)
-
-    def console_prompt(self, newPrompt=None):
-        return self._impl.console_prompt(newPrompt, self._session)
-
     def console_remote(self, host, screen):
         return self._agent.console_remote(host, screen)
-
-    def console_run(self, cmd):
-        return self._impl.console_run(cmd, self._session)
-
-    def console_send(self, data, raw=False):
-        return self._impl.console_send(data, raw, self._session)
 
     def console_toggle(self):
         return self._agent.console_toggle(self._session)
 
-    def console_tail(self):
-        return self._impl.console_tail(self._session)
-
-    def console_wait(self, what, timeout=None):
-        return self._impl.console_wait(what, timeout, self._session)
-
-    def env_get(self, name):
-        return self._impl.env_get(name, self._session)
-
-    def env_set(self, name, value):
-        return self._impl.env_set(name, value, self._session)
-
-    def keyboard_write(self, data):
-        return self._impl.keyboard_write(data, self._session)
-
     def monitor_remote(self, host, screen):
         return self._agent.monitor_remote(host, screen)
-
-    def monitor_send(self, data, raw=False):
-        return self._impl.monitor_send(data, raw, self._session)
-
-    def monitor_wait(self, what, timeout=None):
-        return self._impl.monitor_wait(what, timeout, self._session)
 
     def pastebin_api_key(self):
         return self._agent.pastebin_api_key()
 
     def pastebin_endpoint(self):
         return self._agent.pastebin_endpoint()
-
-    def power_locked(self):
-        return self._impl.power_locked(self._session)
-
-    def storage_bytes_written(self):
-        return self._impl.storage_bytes_written(self._session)
-
-    def storage_close(self):
-        return self._impl.storage_close(self._session)
-
-    def storage_locked(self):
-        return self._impl.storage_locked(self._session)
-
-    def storage_mount(self, part=None):
-        return self._impl.storage_mount(part, self._session)
 
     def storage_network(self, remote):
         cmd = '/usr/sbin/nbd-client'
@@ -189,9 +123,6 @@ class Client:
                     pass
                 else:
                     raise
-
-    def storage_status(self):
-        return self._impl.storage_status(self._session)
 
     def storage_update(self, dest, src=None, callback=None):
         path = dest if src is None else src
@@ -315,15 +246,6 @@ class Client:
             return None
         return bmapDict
 
-    def storage_to_host(self):
-        return self._impl.storage_to_host(self._session)
-
-    def storage_to_target(self):
-        return self._impl.storage_to_target(self._session)
-
-    def storage_swap(self):
-        return self._impl.storage_swap(self._session)
-
     def start(self):
         return self._agent.start()
 
@@ -348,57 +270,6 @@ class Client:
             retries = retries - 1
             time.sleep(60)
         return status
-
-    def target_locked(self):
-        return self._impl.target_locked(self._session)
-
-    def target_off(self):
-        return self._impl.target_off(self._session)
-
-    def target_on(self):
-        return self._impl.target_on(self._session)
-
-    def target_status(self):
-        return self._impl.target_status(self._session)
-
-    def target_toggle(self):
-        return self._impl.target_toggle(self._session)
-
-    def target_unlock(self):
-        return self._impl.target_unlock(self._session)
-
-    def target_uptime(self):
-        return self._impl.target_uptime(self._session)
-
-    def toggle_timestamps(self):
-        return self._impl.toggle_timestamps()
-
-    def usb_find_by_class(self, className):
-        return self._impl.usb_find_by_class(className, self._session)
-
-    def usb_has_class(self, className):
-        return self._impl.usb_has_class(className, self._session)
-
-    def usb_off(self, ndx):
-        return self._impl.usb_off(ndx, self._session)
-
-    def usb_off_by_class(self, className):
-        return self._impl.usb_off_by_class(className, self._session)
-
-    def usb_on(self, ndx):
-        return self._impl.usb_on(ndx, self._session)
-
-    def usb_on_by_class(self, className):
-        return self._impl.usb_on_by_class(className, self._session)
-
-    def usb_ports(self):
-        return self._impl.usb_ports(self._session)
-
-    def usb_status(self, ndx):
-        return self._impl.usb_status(ndx, self._session)
-
-    def usb_toggle(self, ndx):
-        return self._impl.usb_toggle(ndx, self._session)
 
     def version(self):
         return self._agent.version
