siemens/mtda

commit 6bf2e42af84e60d70b77ac6db1789f1ec3f28d09

meta-isar: images: make LAVA an optional feature
Closes: #398
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

meta-isar/recipes-core/images/mtda-image.bb
@@ -65,7 +65,7 @@ IMAGE_PREINSTALL += "                    \
 IMAGE_INSTALL += "python3-hap-python"
 
 # LAVA support
-IMAGE_PREINSTALL += "lava-dispatcher"
+IMAGE_PREINSTALL:lava += "lava-dispatcher"
 
 # Expand root file-system
 IMAGE_INSTALL:append = " expand-on-first-boot "

kas/opt/lava.yml
@@ -1,0 +1,17 @@
+# ---------------------------------------------------------------------------
+# Optional support for LAVA
+# ---------------------------------------------------------------------------
+#
+# This software is a part of MTDA.
+# Copyright (C) 2024 Siemens Digital Industries Software
+#
+# ---------------------------------------------------------------------------
+# SPDX-License-Identifier: MIT
+# ---------------------------------------------------------------------------
+
+header:
+  version: 10
+
+local_conf_header:
+  lava_override: |
+    OVERRIDES .= ":lava"

commit b90410804e9533ac72527bf40828b525081d9c5f

fix(lava): correctly add lava-dispatcher
When enabling the lava override, it completely clears the
IMAGE_PREINSTALL list (except for the lava-dispatcher package). This is
because an incorrect append is used (we append to IMAGE_PREINSTALL:lava
instead of just appending in case lava is selected).

Fixes: 6bf2e42 ("meta-isar: images: make LAVA an optional feature")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

meta-isar/recipes-core/images/mtda-image.bb
@@ -68,7 +68,7 @@ IMAGE_PREINSTALL:remove:ubuntu-noble = "pdudaemon-client"
 IMAGE_INSTALL += "python3-hap-python"
 
 # LAVA support
-IMAGE_PREINSTALL:lava += "lava-dispatcher"
+IMAGE_PREINSTALL:append:lava = " lava-dispatcher"
 
 # Expand root file-system
 IMAGE_INSTALL:append = " expand-on-first-boot "