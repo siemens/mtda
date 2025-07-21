8dc43b10221dc54a7149a52e8e45ec51cfa06f69
Cedric Hombourger <cedric.hombourger@siemens.com>
2025-07-21 10:09:15 +0200

    feat(www): add Storage.toggle() and Target.toggle() to Python
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/assets/www.py
+++ mtda/assets/www.py
@@ -9,8 +9,16 @@
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
-import sys
+import asyncio
 import json
+import uuid
+
+from pyodide.http import pyfetch
+from urllib.parse import urlencode
+
+# ---------------------------------------------------------------------------
+# Helpers for the interpreter
+# ---------------------------------------------------------------------------
 
 
 def get_completions(text):
@@ -19,3 +27,49 @@ def get_completions(text):
         if key.startswith(text):
             completions.append(key)
     return json.dumps(completions)
+
+# ---------------------------------------------------------------------------
+# Implement our Python APIs using REST endpoints
+# ---------------------------------------------------------------------------
+
+
+PY_SESSION_ID = uuid.uuid4()
+
+
+class Storage:
+    async def toggle():
+        """
+        Toggle the shared storage between the HOST and TARGET
+
+        Returns:
+            str: the new state ("HOST", "TARGET" or "???")
+        """
+        response = await Support.get_data("/storage-toggle",
+                                          {"session": PY_SESSION_ID})
+        if 'result' in response:
+            return response['result']['status']
+        return '???'
+
+
+class Support:
+    async def get_data(endpoint, params=None):
+        if params:
+            query = urlencode(params)
+            endpoint = f"{endpoint}?{query}"
+        response = await pyfetch(endpoint, method="GET")
+        return await response.json()
+
+
+class Target:
+    async def toggle():
+        """
+        Toggle the power state of the target.
+
+        Returns:
+            str: the new state ("ON", "OFF" or "???")
+        """
+        response = await Support.get_data("/power-toggle",
+                                          {"session": PY_SESSION_ID})
+        if 'result' in response:
+            return response['result']['status']
+        return '???'
