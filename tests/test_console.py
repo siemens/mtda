5bc4677ed30d5c11e6f1de1433cc630c1c79fd04
Cedric Hombourger <cedric.hombourger@siemens.com>
2025-08-07 07:23:35 +0200

    ci: test console-{head,lines,tail} endpoints
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- tests/test_console.py
+++ tests/test_console.py
@@ -9,6 +9,7 @@
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
+import requests
 import time
 
 from common import Consts
@@ -18,19 +19,39 @@ from mtda.pytest import Console
 from mtda.pytest import Target
 
 
-def test_console_lines(powered_off):
+def do_console_lines(lines):
     # console buffer should be empty
-    lines = Console.lines() == 0
+    status, count = lines()
+    assert status == 200
+    assert count == 0
 
     # Power on and check if the console is getting any data
     assert Target.on() is True
     tries = 30
-    while lines == 0 and tries > 0:
+    while count == 0 and tries > 0:
         Console.send('\x03\r')
-        lines = Console.lines()
+        status, count = lines()
+        assert status == 200
         time.sleep(1)
         tries = tries - 1
-    assert lines > 0
+    assert count > 0
+
+
+def test_console_lines(powered_off):
+    def lines():
+        return 200, Console.lines()
+    do_console_lines(lines)
+
+
+def test_www_console_lines(powered_off):
+    def lines():
+        params = {"session": "pytest"}
+        response = requests.get(f"{Consts.BASE_URL}/console-lines",
+                                params=params)
+        status = response.status_code
+        result = response.json()["result"]["count"] if status == 200 else None
+        return status, result
+    do_console_lines(lines)
 
 
 def test_console_wait_for(powered_on):
@@ -38,15 +59,53 @@ def test_console_wait_for(powered_on):
     assert Console.wait_for("Linux") is not None
 
 
-def test_console_head(powered_on):
+def do_console_head(head):
     cmd = "uname -s\r"
     Console.send(cmd)
     time.sleep(1)
-    assert Console.head().strip() == cmd.strip()
+    status, line = head()
+    assert status == 200
+    assert line.strip() == cmd.strip()
 
 
-def test_console_tail(powered_on):
+def test_console_head(powered_on):
+    def head():
+        return 200, Console.head()
+    do_console_head(head)
+
+
+def test_www_console_head(powered_on):
+    def head():
+        params = {"session": "pytest"}
+        response = requests.get(f"{Consts.BASE_URL}/console-head",
+                                params=params)
+        status = response.status_code
+        result = response.json()["result"]["content"] if status == 200 else None
+        return status, result
+    do_console_head(head)
+ 
+
+def do_console_tail(tail):
     prompt = "shell$ "
     Console.send("export PS1='{}'\r".format(prompt))
     time.sleep(1)
-    assert Utils.escape_ansi(Console.tail()).strip() == prompt.strip()
+    status, line = tail()
+    assert status == 200
+    assert Utils.escape_ansi(line).strip() == prompt.strip()
+
+
+def test_console_tail(powered_on):
+    def tail():
+        return 200, Console.tail()
+    do_console_tail(tail)
+
+
+def test_www_console_tail(powered_on):
+    def tail():
+        params = {"session": "pytest"}
+        response = requests.get(f"{Consts.BASE_URL}/console-tail",
+                                params=params)
+        status = response.status_code
+        result = response.json()["result"]["content"] if status== 200 else None
+        return status, result
+    do_console_tail(tail)
