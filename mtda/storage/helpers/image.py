siemens/mtda

commit 77e6715e3dd0930f95108f93cf35afc15af0d014

fix(bmap): replace bmap writing algorithm with complete on
The current block-based bmap writing algorithm has correctness bugs
around the handling of partial blocks (overlaps). Fixing these would
require special handling of many corner cases. In addition, the current
algorithm is quite slow as often single blocks need to be written.

This patch completely replaces the algorithm with one that is purely
based on index calculations (no corner cases). This further has the
advantage, that chunks as big as possible can be written, reducing
amount of time spent in python code. By that, we get a significant
performance improvement.

To ensure correctness, we also validate the decompressed image data
against the provided checksums. This adds some overhead, especially on
low-powered devices, but we still believe that correctness is more
important than speed. In total, the performance improvements of the
algorithm itself are significantly bigger than the overhead in the
validation.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

mtda/storage/helpers/image.py
@@ -32,8 +32,9 @@ class Image(StorageController):
         self.isfuse = False
         self.isloop = False
         self.bmapDict = None
-        self.lastBlockIdx = 0
         self.crtBlockRange = 0
+        self.writtenBlocks = 0
+        self.writtenBytes = 0
         self.overlap = 0
         self.rangeChkSum = None
         self.lock = threading.Lock()
@@ -290,7 +291,6 @@ class Image(StorageController):
     def setBmap(self, bmapDict):
         self.bmapDict = bmapDict
         self.crtBlockRange = 0
-        self.lastBlockIdx = 0
         self.overlap = 0
         self.rangeChkSum = self._get_hasher_by_name()
 
@@ -357,95 +357,49 @@ class Image(StorageController):
         if self.handle is not None:
             # Check if there is a valid bmapDict, write all data otherwise
             if self.bmapDict is not None:
-                result = 0
-                datalen = len(data)
-                blksz = self.bmapDict["BlockSize"]
-                cRange = self.bmapDict["BlockMap"][self.crtBlockRange]
-                # This happens at xz,bz2 and gz compression
-                # since the decompressed chunk can be smaller than a block
-                if datalen < blksz:
-                    # If there is overlap and the new data is enough
-                    # to form a block, write it
-                    if self.overlap + datalen >= blksz:
-                        dataBlockWritten = \
-                            self.handle.write(data[0:(blksz-self.overlap)])
-                        result += dataBlockWritten
-                        self.lastBlockIdx += 1
-                        # Write the rest of the data
-                        rest = self.handle.write(data[dataBlockWritten:])
-                        result += rest
-                        self.overlap = rest
-                        self.mtda.debug(3, "storage.helpers.image.write(): %s"
-                                        % str(result))
-                        self.lock.release()
-                        return result
-                    else:
-                        # Not enough data to even write a block
-                        # write it and add to the overlap
-                        subBlockWritten = self.handle.write(data)
-                        result += subBlockWritten
-                        self.overlap += subBlockWritten
-                        self.mtda.debug(3, "storage.helpers.image.write(): %s"
-                                        % str(result))
-                        self.lock.release()
-                        return result
-                # Complete partially written last block,
-                # start blockpointer depending on this
-                if self.overlap:
-                    result += self.handle.write(data[0:(blksz-self.overlap)])
-                    self.lastBlockIdx += 1
-                    b = blksz-self.overlap
-                else:
-                    b = 0
-
-                while b < datalen:
-                    if self.lastBlockIdx > cRange['last']:
-                        self.crtBlockRange += 1
-                        cRange = \
-                            self.bmapDict["BlockMap"][self.crtBlockRange]
-                    flBlks2Write = 1
-                    advanceBlocks = 1
-                    if self.lastBlockIdx >= cRange['first']:
-                        flBlks2Write = int((datalen-b)/blksz)
-                        # Write multiple blocks if possible in that range
-                        if (self.lastBlockIdx + flBlks2Write) < cRange['last']:
-                            # Check if it is a full Block
-                            if flBlks2Write:
-                                result += self.handle.write(
-                                    data[b:b+flBlks2Write*blksz])
-                                advanceBlocks = flBlks2Write
-                            else:
-                                result += self.handle.write(data[b:b+blksz])
-                                # Needed for incrementing dynamic blockpointer
-                                flBlks2Write = 1
-                        else:
-                            result += self.handle.write(data[b:b+blksz])
-                            flBlks2Write = 1
-                    else:
-                        # If there is a block to skip use the opportunity
-                        # to also drop the overlap, skip the subBlock
-                        # This allows for faster write-times
-                        # since the time consuming self.handle.write()
-                        # does not need to be called at the start
-                        # when there is no overlap anymore
-                        if self.overlap:
-                            b += self.overlap
-                            self.handle.seek(self.overlap, io.SEEK_CUR)
-                            self.overlap = 0
-                        self.handle.seek(min(datalen-b, blksz), io.SEEK_CUR)
-                    # Only increment block index in case we wrote full blocks
-                    if b + blksz*flBlks2Write <= datalen:
-                        self.lastBlockIdx += advanceBlocks
-                    else:
-                        self.overlap = datalen - b
-                    # Increase blockpointer
-                    b += flBlks2Write * blksz
+                result = self._write_with_bmap(data)
             else:
                 # No bmap
                 result = self.handle.write(data)
         self.mtda.debug(3, "storage.helpers.image.write(): %s" % str(result))
         self.lock.release()
         return result
+
+    def _write_with_bmap(self, data):
+        cur_range = self.bmapDict["BlockMap"][self.crtBlockRange]
+        blksize = self.bmapDict["BlockSize"]
+        # offset within the data buffer
+        offset = 0
+        # remaining bytes in data buffer
+        remaining = len(data) - offset
+        while remaining:
+            if self.writtenBlocks > cur_range["last"]:
+                self._validate_and_reset_range()
+                self.crtBlockRange += 1
+                cur_range = self.bmapDict["BlockMap"][self.crtBlockRange]
+
+            if self.writtenBlocks >= cur_range["first"]:
+                # bmap ranges are inclusive. Exclusive ranges like [from, to)
+                # are easier to handle, hence add one block
+                end = min(remaining,
+                          (cur_range["last"] - self.writtenBlocks + 1)
+                          * blksize - self.overlap)
+                nbytes = self._write_with_chksum(data[offset:offset + end])
+            else:
+                # the range already got incremented, hence we need to iterate
+                # until the begin of the current range
+                nbytes = min(remaining,
+                             (cur_range["first"] - self.writtenBlocks)
+                             * blksize - self.overlap)
+                self.handle.seek(nbytes, io.SEEK_CUR)
+            self.overlap -= min(self.overlap, nbytes)
+            self.writtenBytes += nbytes
+            self.writtenBlocks = self.writtenBytes // blksize
+            offset += nbytes
+            remaining -= nbytes
+
+        self.overlap = self.writtenBytes % blksize
+        return offset
 
     def _validate_and_reset_range(self):
         if not self.rangeChkSum:

commit d9c1550ba952fd219c54c81ff2c6ffe18c125eea

fix(storage): validate checksum of last range as well
We currently only validate a range once it gets incremented. However, in
case of the last range, we do not increment, as this would lead to an
out-of-bounds error in the bmap dict.

We fix this by splitting the range-bump part and the validate range
part. We now validate directly after finishing a range and bump on the
next iteration.

Fixes: 77e6715 ("fix(bmap): replace bmap writing algorithm with ...")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

mtda/storage/helpers/image.py
@@ -346,17 +346,17 @@ class Image(StorageController):
         while remaining:
             writtenBlocks = self.writtenBytes // blksize
             if writtenBlocks > cur_range["last"]:
-                self._validate_and_reset_range()
                 self.crtBlockRange += 1
                 cur_range = self.bmapDict["BlockMap"][self.crtBlockRange]
 
             if writtenBlocks >= cur_range["first"]:
                 # bmap ranges are inclusive. Exclusive ranges like [from, to)
                 # are easier to handle, hence add one block
-                end = min(remaining,
-                          (cur_range["last"] - writtenBlocks + 1)
-                          * blksize - self.overlap)
+                cur_last_block = cur_range["last"] + 1
+                end = min(remaining, (cur_last_block - writtenBlocks) * blksize - self.overlap)
                 nbytes = self._write_with_chksum(data[offset:offset + end])
+                if self.writtenBytes + nbytes == cur_last_block * blksize:
+                    self._validate_and_reset_range()
                 self.mtda.notify_write(size=nbytes, mapped=self.mappedBytes)
             else:
                 # the range already got incremented, hence we need to iterate