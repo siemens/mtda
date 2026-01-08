siemens/mtda

commit 02dd6b0bf7012d4eabd5dfdb255195a48e11ef3b

feat(storage): add initial support for commit/rollback to usbf
Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

mtda/storage/helpers/image.py
@@ -133,6 +133,8 @@ class Image(StorageController):
         p = pathlib.Path(self.file)
         if p.is_block_device() is True:
             self.device = self.file
+            cmd = ['/sbin/kpartx', '-av', self.device]
+            subprocess.check_call(cmd)
             result = True
         else:
             cmd = ["losetup", "-f", "--show", "-P", self.file]
@@ -225,15 +227,16 @@ class Image(StorageController):
     def open(self):
         self.mtda.debug(3, "storage.helpers.image.open()")
         with self.lock:
-            result = self._open_impl()
+            result = True
+            if self._status() == CONSTS.STORAGE.ON_HOST:
+                if self.handle is None:
+                    result = self._open()
         self.mtda.debug(3, f"storage.helpers.image.open(): {str(result)}")
         return result
 
-    def _open_impl(self):
-        if self._status() == CONSTS.STORAGE.ON_HOST:
-            if self.handle is None:
-                self.handle = open(self.file, "r+b")
-                self.handle.seek(0, 0)
+    def _open(self):
+        self.handle = open(self.file, "r+b")
+        self.handle.seek(0, 0)
         return True
 
     def path(self):

mtda/storage/usbf.py
@@ -10,10 +10,12 @@
 # ---------------------------------------------------------------------------
 
 # System imports
+import atexit
 import os
 import stat
 import subprocess
 import pathlib
+import time
 
 # Local imports
 import mtda.constants as CONSTS
@@ -22,27 +24,46 @@ from mtda.support.usb import Composite
 from mtda.utils import SystemdDeviceUnit
 
 
+# Local constants
+DM_BASE = "mtda-base"
+DM_COW = "mtda-cow"
+
+
 class UsbFunctionController(Image):
 
     def __init__(self, mtda):
         super().__init__(mtda)
-        self.device = None
+        self.user_device = None
+        self.user_file = None
+        self.base_device = None
+        self.cow_device = None
+        self.loop_device = None
         self.file = None
-        self.loop = None
         self.mode = CONSTS.STORAGE.ON_HOST
         Composite.mtda = mtda
 
     def cleanup(self):
         self.mtda.debug(3, "storage.usbf.cleanup()")
-
         result = None
-        if self.loop is not None:
-            self.mtda.debug(2, f"storage.usbf.cleanup(): removing {self.loop}")
-            cmd = ['/usr/sbin/losetup', '-d', self.loop]
-            self.loop = subprocess.check_call(cmd)
+
+        self.cleanup_cow()
+        if self.loop_device is not None:
+            self.mtda.debug(2, "storage.usbf.cleanup(): "
+                               f"removing {self.loop_device}")
+            cmd = ['/usr/sbin/losetup', '-d', self.loop_device]
+            subprocess.check_call(cmd)
+            self.loop_device = None
 
         self.mtda.debug(3, f"storage.usbf.cleanup(): {result}")
         return result
+
+    def cleanup_cow(self):
+        if os.path.exists(f"/dev/mapper/{DM_COW}"):
+            subprocess.run(['/sbin/kpartx', '-dv', f"/dev/mapper/{DM_COW}"])
+            subprocess.run(['/sbin/dmsetup', 'remove', DM_COW])
+
+        if os.path.exists(f"/dev/mapper/{DM_BASE}"):
+            subprocess.run(['/sbin/dmsetup', 'remove', DM_BASE])
 
     """ Configure this storage controller from the provided configuration"""
     def configure(self, conf):
@@ -50,67 +71,176 @@ class UsbFunctionController(Image):
 
         result = False
         if 'device' in conf:
-            self.device = conf['device']
+            self.user_device = conf['device']
+        if 'cow' in conf:
+            self.cow_device = conf['cow']
         if 'file' in conf:
-            self.file = conf['file']
-            d = os.path.dirname(self.file)
+            self.user_file = conf['file']
+            d = os.path.dirname(self.user_file)
             os.makedirs(d, mode=0o755, exist_ok=True)
-            if not os.path.exists(self.file):
-                sparse = pathlib.Path(self.file)
+            if not os.path.exists(self.user_file):
+                sparse = pathlib.Path(self.user_file)
                 sparse.touch()
                 os.truncate(str(sparse), CONSTS.IMAGE_FILESIZE)
 
-        if self.device is not None and self.file is not None:
-            self.mtda.debug(1, "storage.usbf.configure(): "
-                               "both 'file' ({}) and 'device' ({}) are set, "
-                               "using 'file'".format(self.file, self.device))
-            self.device = None
-        elif self.device is None:
-            import atexit
-            cmd = ['/usr/sbin/losetup', '-f', '--show', self.file]
-            self.loop = subprocess.check_output(cmd).decode("utf-8").strip()
-            atexit.register(self.cleanup)
-            self.mtda.debug(2, "storage.usbf.configure(): created loopback "
-                               "device {} for {}".format(self.loop, self.file))
-        else:
-            self.file = self.device
+        if self.user_device is None and self.user_file is None:
+            raise RuntimeError("shared storage device/file not defined!")
 
-        if self.file is not None:
-            result = Composite.configure('storage', conf)
+        if self.user_device is not None and self.user_file is not None:
+            self.mtda.debug(1, "storage.usbf.configure(): "
+                               f"both 'file' ({self.user_file}) and "
+                               f"'device' ({self.user_device}) are set, "
+                               "using 'file'")
+            self.user_device = None
+
+        self.file = self.user_device
+        if self.user_file:
+            cmd = ['/usr/sbin/losetup', '-f', '--show', self.user_file]
+            self.loop_device = subprocess.check_output(cmd, text=True).strip()
+            self.file = self.loop_device
+            self.mtda.debug(2, "storage.usbf.configure(): created loopback "
+                               f"device {self.loop_device} for "
+                               f"{self.user_file}")
+
+        self.base_device = None
+        if self.cow_device:
+            self.configure_cow(conf)
+
+        if self.user_file or self.cow_device:
+            atexit.register(self.cleanup)
+
+        self.mtda.debug(3, "storage.usbf.configure(): "
+                           f"will use {self.file}")
+        conf['_device_'] = self.file
+        result = Composite.configure('storage', conf)
 
         self.mtda.debug(3, f"storage.usbf.configure(): {result}")
         return result
 
+    def configure_cow(self, conf):
+        self.base_device = self.file
+
+        self.cleanup_cow()
+
+        cmd = ['/sbin/blockdev', '--getsz', self.base_device]
+        self.base_size = subprocess.check_output(cmd, text=True).strip()
+
+        cmd = ['/sbin/dmsetup', 'create', DM_BASE, '--table',
+               f"0 {self.base_size} snapshot-origin {self.base_device}"]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'create', DM_COW, '--table',
+               f"0 {self.base_size} snapshot "
+               f"{self.base_device} {self.cow_device} P 8"]
+        subprocess.check_call(cmd)
+        self.file = f"/dev/mapper/{DM_COW}"
+
     def configure_systemd(self, dir):
-        if self.file is None or os.path.exists(self.file) is False:
-            return
-        mode = os.stat(self.file).st_mode
-        if stat.S_ISBLK(mode) is False:
+        if self.user_device is None or \
+           os.path.exists(self.user_device) is False:
             return
         dropin = os.path.join(dir, 'auto-dep-storage.conf')
-        SystemdDeviceUnit.create_device_dependency(dropin, self.file)
+        SystemdDeviceUnit.create_device_dependency(dropin, self.user_device)
+
+        if self.cow_device is None or os.path.exists(self.cow_device) is False:
+            return
+        dropin = os.path.join(dir, 'auto-dep-storage-cow.conf')
+        SystemdDeviceUnit.create_device_dependency(dropin, self.cow_device)
+
+    def rollback(self):
+        if self.cow_device is None:
+            raise FileNotFoundError('no CoW device was configured!')
+
+        subprocess.run(['/sbin/kpartx', '-dv', f"/dev/mapper/{DM_COW}"])
+        cmd = ['/sbin/dmsetup', 'remove', DM_COW]
+        subprocess.check_call(cmd)
+
+        cmd = ['/bin/dd', 'if=/dev/zero', f'of={self.cow_device}',
+               'bs=1M', 'count=4', 'conv=fsync']
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'create', DM_COW, '--table',
+               f"0 {self.base_size} snapshot "
+               f"{self.base_device} {self.cow_device} P 8"]
+        subprocess.check_call(cmd)
+
+    def commit(self):
+        if self.cow_device is None:
+            raise FileNotFoundError('no CoW device was configured!')
+
+        # Trigger merge
+        cmd = ['/sbin/dmsetup', 'suspend', DM_BASE]
+        subprocess.check_call(cmd)
+
+        subprocess.run(['/sbin/kpartx', '-dv', f"/dev/mapper/{DM_COW}"])
+        cmd = ['/sbin/dmsetup', 'remove', DM_COW]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'reload', DM_BASE, '--table',
+               f"0 {self.base_size} snapshot-merge "
+               f"{self.base_device} {self.cow_device} P 8"]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'resume', DM_BASE]
+        subprocess.check_call(cmd)
+
+        # Wait for merge to complete
+        while True:
+            cmd = ['/sbin/dmsetup', 'status', DM_BASE]
+            status = subprocess.check_output(cmd, text=True).strip()
+
+            if 'snapshort-merge' not in status:
+                break
+
+            status = status.split()
+            allocated = status[3].split('/')[0]
+            metadata = status[4]
+
+            self.mtda.debug(4, "storage.usbf.commit(): "
+                               f"allocated={allocated}, metadata={metadata}")
+            if allocated == metadata:
+                break
+
+            time.sleep(1000)
+
+        # Resume CoW device
+        cmd = ['/sbin/dmsetup', 'suspend', DM_BASE]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'reload', DM_BASE, '--table',
+               f"0 {self.base_size} snapshot-origin {self.base_device}"]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'resume', DM_BASE]
+        subprocess.check_call(cmd)
+
+        cmd = ['/sbin/dmsetup', 'create', DM_COW, '--table',
+               f"0 {self.base_size} snapshot "
+               f"{self.base_device} {self.cow_device} P 8"]
+        subprocess.check_call(cmd)
 
     """ Get file used by the USB Function driver"""
     def probe(self):
         self.mtda.debug(3, "storage.usbf.probe()")
 
         result = False
-        if self.device is not None:
-            if os.path.exists(self.device) is True:
-                mode = os.stat(self.device).st_mode
+        if self.user_device is not None:
+            if os.path.exists(self.user_device) is True:
+                mode = os.stat(self.user_device).st_mode
                 if stat.S_ISBLK(mode) is False:
                     self.mtda.debug(1, "storage.usbf.probe(): "
-                                       "{} is not a block "
-                                       "device!".format(self.device))
+                                       f"{self.user_device} is not a block "
+                                       "device!")
+
         if self.file is not None:
             if os.path.exists(self.file) is True:
                 result = True
             else:
-                self.mtda.debug(1, "storage.usbf."
-                                   f"probe(): {self.file} not found!")
+                self.mtda.debug(1, "storage.usbaf.probe(): "
+                                   f"{self.file} not found!")
         else:
             self.mtda.debug(1, "storage.usbf.probe(): "
-                               "file not configured!")
+                               "storage device/file not configured!")
 
         self.mtda.debug(3, f"storage.usbf.probe(): {result}")
         return result
@@ -141,6 +271,27 @@ class UsbFunctionController(Image):
 
         self.lock.release()
         self.mtda.debug(3, f"storage.usbf.to_target(): {result}")
+        return result
+
+    def _open(self):
+        self.mtda.debug(3, "storage.usbf._open()")
+
+        result = True
+        path = None
+        if self.cow_device:
+            self.rollback()
+            path = f"/dev/mapper/{DM_BASE}"
+        elif self.loop_device:
+            path = self.loop_device
+        elif self.user_device:
+            path = self.user_device
+
+        self.mtda.debug(3, "storage.usbf._open(): "
+                           f"opening {path}")
+        self.handle = open(path, "r+b")
+        self.handle.seek(0, 0)
+
+        self.mtda.debug(3, f"storage.usbf._open(): {result}")
         return result
 
     """ Determine where the shared storage device is attached"""

debian/control
@@ -20,6 +20,7 @@ Multi-Arch: foreign
 Depends: mtda-common,
          dnsmasq-base,
          iptables,
+         kpartx,
          python3-daemon,
          python3-gevent,
          python3-libgpiod,

mtda-cli
@@ -304,15 +304,33 @@ class Application:
 
     def storage_cmd(self, args):
         cmds = {
+           'commit': self.storage_commit,
            'host': self.storage_host,
            'mount': self.storage_mount,
            'network': self.storage_network,
+           'rollback': self.storage_rollback,
            'target': self.storage_target,
            'update': self.storage_update,
            'write': self.storage_write
         }
 
         return cmds[args.subcommand](args)
+
+    def storage_commit(self, args=None):
+        status = self.client().storage_commit()
+        if status is False:
+            print("could not commit changes made to shared storage!",
+                  file=sys.stderr)
+            return 1
+        return 0
+
+    def storage_rollback(self, args=None):
+        status = self.client().storage_rollback()
+        if status is False:
+            print("could not rollback changes made to shared storage!",
+                  file=sys.stderr)
+            return 1
+        return 0
 
     def storage_host(self, args=None):
         status = self.client().storage_to_host()
@@ -734,6 +752,12 @@ class Application:
         p.set_defaults(func=cmd)
         subsub = p.add_subparsers(dest="subcommand")
         subsub.required = True
+        s = subsub.add_parser(
+            "commit", help="Commit changes made to shared storage device"
+        )
+        s = subsub.add_parser(
+            "rollback", help="Rollback changes made to shared storage device"
+        )
         s = subsub.add_parser(
             "host", help="Attach the shared storage device to the host"
         )

mtda/main.py
@@ -737,6 +737,40 @@ class MultiTenantDeviceAccess:
         return result
 
     @Pyro4.expose
+    def storage_commit(self, **kwargs):
+        self.mtda.debug(3, "main.storage_commit()")
+
+        session = kwargs.get("session", None)
+        self.session_ping(session)
+
+        result = False
+        if self.storage is not None:
+            if self.storage_locked(session):
+                raise RuntimeError('cannot commit changes, '
+                                   'storage is locked!')
+            result = self.storage.commit()
+
+        self.mtda.debug(3, f"main.storage_commit(): {result}")
+        return result
+
+    @Pyro4.expose
+    def storage_rollback(self, **kwargs):
+        self.mtda.debug(3, "main.storage_rollback()")
+
+        session = kwargs.get("session", None)
+        self.session_ping(session)
+
+        result = False
+        if self.storage is not None:
+            if self.storage_locked(session):
+                raise RuntimeError('cannot rollback changes, '
+                                   'storage is locked!')
+            result = self.storage.rollback()
+
+        self.mtda.debug(3, f"main.storage_rollback(): {result}")
+        return result
+
+    @Pyro4.expose
     def storage_flush(self, size, **kwargs):
         self.mtda.debug(3, f"main.storage_flush({size})")
 
@@ -1536,6 +1570,8 @@ class MultiTenantDeviceAccess:
                         if hasattr(self, hook):
                             postconf = getattr(self, hook)
                         self.load_subsystem(sub, parser, postconf)
+                    except RuntimeError:
+                        self.error(f"configuration of {sub} failed!")
                     except configparser.NoOptionError:
                         self.error(f"variant not defined for '{sub}'!")
                     except ImportError:

mtda/storage/controller.py
@@ -85,3 +85,9 @@ class StorageController(object):
     def write(self, data):
         """ Write data to the shared storage device"""
         return False
+
+    def commit(self):
+        raise RuntimeError('commit is not supported')
+
+    def rollback(self):
+        raise RuntimeError('rollback is not supported')

mtda/support/usb.py
@@ -70,11 +70,7 @@ class Composite:
     def _configure(what, conf):
         result = True
         if what == 'storage':
-            # keep this logic in sync with usbf handler (prefer file)
-            if 'file' in conf:
-                Composite._storage = conf['file']
-            elif 'device' in conf:
-                Composite._storage = conf['device']
+            Composite._storage = conf['_device_']
         if what in Composite.functions:
             Composite.functions[what]['configured'] = True
             Composite.functions[what]['enabled'] = True

commit f59ed4856497ca844cec328dc9046ba4ac497aff

fix(usbf): ignore missing CoW device on close
When closing the image, we currently only check if the rollback feature
is implemented, but not if it is configured. On QEMU this is fine, as
the rollback implementation itself unconditionally can be executed (if
not configured, the call is a noop). However, on usbf the call checks if
a CoW device is configured and raises otherwise.

As we cannot check if a device is configured at the callsite in image,
we introduce a parameter to the rollback method to ignore missing
devices. For the sake of completeness, we implement the same also for
the commit method.

Closes: #560
Fixes: 02dd6b0b ("feat(storage): add initial support for commit/rollback")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

mtda/storage/helpers/image.py
@@ -69,7 +69,7 @@ class Image(StorageController):
             self.handle = None
             self.bmapDict = None
             if hasattr(self, 'rollback'):
-                self.rollback()
+                self.rollback(ignore_missing=True)
             try:
                 subprocess.check_output(["sync"])
             except subprocess.CalledProcessError:

mtda/storage/qemu.py
@@ -102,12 +102,12 @@ class QemuController(Image):
         self.lock.release()
         return result
 
-    def commit(self):
+    def commit(self, ignore_missing=False):
         if self.cow:
             cmd = ['qemu-img', 'commit', self.cow]
             subprocess.check_call(cmd)
 
-    def rollback(self):
+    def rollback(self, ignore_missing=False):
         if self.cow:
             cmd = ['qemu-img', 'create', '-F', 'raw', '-f', 'qcow2',
                    '-b', self.file, self.cow, f'{self.size}M']

mtda/storage/usbf.py
@@ -147,8 +147,10 @@ class UsbFunctionController(Image):
         dropin = os.path.join(dir, 'auto-dep-storage-cow.conf')
         SystemdDeviceUnit.create_device_dependency(dropin, self.cow_device)
 
-    def rollback(self):
+    def rollback(self, ignore_missing=False):
         if self.cow_device is None:
+            if ignore_missing:
+                return
             raise MissingCowDeviceError('rollback')
 
         subprocess.run(['/sbin/kpartx', '-dv', f"/dev/mapper/{DM_COW}"])
@@ -164,8 +166,10 @@ class UsbFunctionController(Image):
                f"{self.base_device} {self.cow_device} P 8"]
         subprocess.check_call(cmd)
 
-    def commit(self):
+    def commit(self, ignore_missing=False):
         if self.cow_device is None:
+            if ignore_missing:
+                return
             raise MissingCowDeviceError('commit')
 
         # Trigger merge