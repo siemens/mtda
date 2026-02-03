bf906793480fe9ce9f2b36dfb44a730656063e0e
Cedric Hombourger <cedric.hombourger@siemens.com>
2026-02-03 08:50:58 +0100

    debian: changelog for version 0.45
    Signed-off-by: Cedric Hombourger <cedric.hombourger@siemens.com>

--- mtda/__version__.py
+++ mtda/__version__.py
@@ -3,12 +3,12 @@
 # ---------------------------------------------------------------------------
 #
 # This software is a part of MTDA.
-# Copyright (C) 2025 Siemens AG
+# Copyright (C) 2026 Siemens AG
 #
 # ---------------------------------------------------------------------------
 # SPDX-License-Identifier: MIT
 # ---------------------------------------------------------------------------
 
 __license__ = 'MIT'
-__copyright__ = 'Copyright (C) 2025 Siemens AG'
-__version__ = '0.44'
+__copyright__ = 'Copyright (C) 2026 Siemens AG'
+__version__ = '0.45'
