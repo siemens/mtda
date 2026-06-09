siemens/mtda

commit bba9ae67fd686e9b24a8468ebe9be08ba39a9a8c

feat(usbf): auto-create file if not available
In case a file is specified, but the file does not exist, create it.
This brings this code in line with the Qemu image controller.

In Qemu the file-size is hard-coded. We now move that to a const and use
it in both qemu and usbf controllers.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

mtda/storage/usbf.py
@@ -13,6 +13,7 @@
 import os
 import stat
 import subprocess
+import pathlib
 
 # Local imports
 import mtda.constants as CONSTS
@@ -52,6 +53,12 @@ class UsbFunctionController(Image):
             self.device = conf['device']
         if 'file' in conf:
             self.file = conf['file']
+            d = os.path.dirname(self.file)
+            os.makedirs(d, mode=0o755, exist_ok=True)
+            if not os.path.exists(self.file):
+                sparse = pathlib.Path(self.file)
+                sparse.touch()
+                os.truncate(str(sparse), CONSTS.IMAGE_FILESIZE)
 
         if self.device is not None and self.file is not None:
             self.mtda.debug(1, "storage.usbf.configure(): "

mtda/constants.py
@@ -25,6 +25,7 @@ class DEFAULTS:
     WWW_HOST = '127.0.0.1'
     WWW_PORT = 5000
     HID_KEYBOARD_DEVICE = "/dev/hidg0"
+    IMAGE_FILESIZE = 8*1024*1024*1024
 
 
 class EVENTS:

mtda/storage/qemu.py
@@ -41,7 +41,7 @@ class QemuController(Image):
         if os.path.exists(self.file) is False:
             sparse = pathlib.Path(self.file)
             sparse.touch()
-            os.truncate(str(sparse), 8*1024*1024*1024)
+            os.truncate(str(sparse), CONSTS.IMAGE_FILESIZE)
         if 'name' in conf:
             self.name = conf['name']
 

commit 4411781c8394a958bae31355ccd608ae6812739b

fix(mtda): use correct default constant in usbf storage
A similar fix has been applied to the qemu storage in b02521ff.

Fixes: bba9ae67 ("feat(usbf): auto-create file if not available")
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>

mtda/storage/usbf.py
@@ -81,7 +81,7 @@ class UsbFunctionController(Image):
             if not os.path.exists(self.user_file):
                 sparse = pathlib.Path(self.user_file)
                 sparse.touch()
-                os.truncate(str(sparse), CONSTS.IMAGE_FILESIZE)
+                os.truncate(str(sparse), CONSTS.DEFAULTS.IMAGE_FILESIZE)
 
         if self.user_device is None and self.user_file is None:
             raise RuntimeError("shared storage device/file not defined!")